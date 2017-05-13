ab488bbc5f4791621caa4bbe9695a739ed15720f
Brian McKeown <bmckeown2011@gmail.com>
2017-05-13 09:39:22 -0400

    Uploaded existing files to github

---
+++ js/map.markers.js
@@ -0,0 +1,17 @@
+//List of map markers
+var map_markers = [
+	{
+		"title": "Wedding",
+		"latitude":42.244364,
+		"longitude":-72.1754177,
+		"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Salem Cross Inn<br>260 W Main St, West Brookfield, MA"
+	},
+	{
+		"title": "Hotel & After Party",
+		"latitude":42.1135807,
+		"longitude":-72.090755,
+		"icon":"fa-glass", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Sturbridge Host Hotel<br>366 Main St, Sturbridge, MA"
+	},
+];
---
+++ js/variables.js
@@ -0,0 +1,34 @@
+/**
+ * Javascript template variables
+ */
+
+var	mobileMenuTitle = "Menu",					//The title of the mobile menu
+	
+	hero100PercentHeight = false,				//If true, the hero section (home) will be set with a minimum height of 100% window height. If false, hero height will be the height of its content.
+	
+	//TWITTER VARIABLE
+	twitter_username = "Envato",				//Replace with your own Twitter username
+	
+	
+	//GOOGLE MAP VARIABLES
+	map_canvas_id = "map_canvas",				//The HTML "id" of the map canvas
+	map_color = "#ad9fb3",						//Google map color
+	map_initial_zoom = 10,						//The initial zoom when Google map loads
+	map_initial_latitude = 42.244364,			//Google map initial Latitude. If "null", the latitude of the first marked will be used
+	map_initial_longitude = -72.1754177,		//Google map initial Longitude. If "null", the longitude of the first marked will be used
+	use_default_map_style = true,				//If true, default map style will be used
+	
+	
+	//CONTACT FORM VARIABLES
+	contact_form_success_msg = "Not yet available :( Check back at a later date." //Form successfully submitted! :)"
+	,
+	contact_form_error_msg = "Not yet available :( Check back at a later date."//Error sending message :(
+	,
+	
+	
+	//COUNTDOWN VARIABLES
+	c_days = "DAYS",							//Countdown "Days" label
+	c_hours = "HOURS",							//Countdown "Hours" label
+	c_minutes = "MIN.",							//Countdown "Minutes" label
+	c_seconds = "SEC.",							//Countdown "Seconds" label
+	countdownEndMsg = "Event Started!";			//Message to display when the countdown reaches the end
